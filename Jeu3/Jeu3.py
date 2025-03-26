304b74de65c2c66c578bcc3d0407c095f00c12d4
Moon_suny <maximelortal@gmail.com>
2025-03-26 12:46:44 +0100

    Avancé jeu3 , ajout de gravité

--- Jeu3/Jeu3.py
+++ Jeu3/Jeu3.py
@@ -1,11 +1,16 @@
 import pygame 
 from pygame.locals import *
+import threading
+from time import sleep
 
 # Initialisation de Pygame
 pygame.init()
 
 screen_width = 1000
 screen_height = 800
+
+
+
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Jeu')
@@ -16,9 +21,9 @@ Background_img = pygame.image.load('Jeu3/img/Backgrnd.png')
 roue_img = pygame.image.load('Jeu3/img/Roue.gif')
 
 # Création du sol (blanc pour le masque, invisible pour l'affichage)
-sol_x, sol_y = 0, screen_height - 50  # Position en bas de l'écran
-sol_surface = pygame.Surface((1000, 50), pygame.SRCALPHA)
-sol_surface.fill((255, 255, 255))  # Blanc = partie solide pour le masque
+sol_x, sol_y = -500, screen_height - 50  # Position en bas de l'écran
+sol_surface = pygame.Surface((2000, 50), pygame.SRCALPHA)
+sol_surface.fill((255, 255, 255))  # Blanc
 sol_mask = pygame.mask.from_surface(sol_surface)
 
 # Chargement du joueur
@@ -31,6 +36,21 @@ player_img = pygame.transform.scale(player_img, (50, 50))
 player_mask = pygame.mask.from_surface(player_img)
 barre_mask = pygame.mask.from_surface(barre_img)
 
+
+
+# Création d'un thread pour la gravité
+
+def threaded_function(collisions):
+    global player_y 
+    global running
+    while running:
+        sleep(0.1)
+        if collisions == False:
+            player_y += 1
+        sleep(1)
+        
+
+
 # Boucle de jeu
 running = True
 while running:
@@ -38,8 +58,20 @@ while running:
     screen.blit(barre_img, (0, 0))
     screen.blit(sol_surface, (sol_x, sol_y))  # Dessiner le sol en bas
 
+    #Gravitée
+    Start = False
+    collision = False
+    gravite = threading.Thread(target=threaded_function, args=(collision,))
+    if Start == False:
+        Start = True
+        gravite.start()
+    else:
+        pass
+
+
     for event in pygame.event.get():
         if event.type == QUIT:
+            running = False
             pygame.quit()
             exit()
 
@@ -51,8 +83,7 @@ while running:
         player_x += player_speed
     if keys[pygame.K_UP]: 
         player_y -= player_speed
-    if keys[pygame.K_DOWN]: 
-        player_y += player_speed
+
 
     # Collision 
     offset_x = sol_x - player_x  # Décalage horizontal entre le sol et le joueur
@@ -60,16 +91,23 @@ while running:
 
     if barre_mask.overlap(player_mask, (player_x, player_y)) != None:
         collision = True
-        if (player_x, player_y) < barre_mask.overlap(player_mask, (player_x, player_y)) :
+
+        contactX,contactY = barre_mask.overlap(player_mask, (player_x, player_y))
+        if  player_x < contactX:
             player_x -= player_speed
+        if player_y < contactY:
             player_y -= player_speed
+
     elif player_mask.overlap(sol_mask, (offset_x, offset_y)):
         collision = True
-        player_y -= player_speed
-    else:
-        collision = False
         player_y += player_speed
+    else:
+        #collision = False
+        pass
 
+
+    #gravité
+    
 
     # Afficher le joueur
     screen.blit(player_img, (player_x, player_y))
