a0478873dfa96a665ef13f5a8a686428e8f23693
piloi <pierreeloiblanchier@gmail.com>
2025-03-26 11:16:03 +0100

    petit changement de jeu2

---
+++ Jeu2/mainjeu2.py
@@ -0,0 +1,12 @@
+import pygame
+pygame.init()
+
+pygame.display.set_mode((800, 600))
+pygame.display.set_caption("enfuis toi !")
+
+running = True
+while running :
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
