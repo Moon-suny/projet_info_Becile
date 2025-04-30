653cdb291f43a6ed75e5df82ad7264ea11c9a929
clemsc75 <clemsc77@gmail.com>
2025-04-30 11:37:25 +0200

    Ajout de la gestion des chemins pour les modules de sauvegarde et mise à jour du fichier .gitignore pour ignorer les fichiers .pyc.

--- Menu_du_jeu/Menu.py
+++ Menu_du_jeu/Menu.py
@@ -4,8 +4,11 @@ import pygame
 
 # Chemin vers le répertoire parent
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../Jeu1")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../Fch_Save")))
 
+# Importer les modules nécessaires
 from lib_jeu1_pygame import create_button, check_button_clicked, DEFAULT_BUTTON_COLOR # Importer les fonctions de gestion des boutons
+from lib_save_interaction import * # Importer les fonctions de sauvegarde et de chargement de partie
 
 # Initialisation de Pygame
 pygame.init()
