bb595226fac5aa1b99f5d00846ed406cbf914da9
Moon_suny <maximelortal@gmail.com>
2025-05-02 00:14:10 +0200

    Implement code changes to enhance functionality and improve performance

--- Cinematique/Dialogues/Dialogue-2.py
+++ Cinematique/Dialogues/Dialogue-2.py
@@ -1,5 +1,12 @@
 import pygame
 import pygame_gui
+import os
+
+def lancementJ2():
+    os.path.dirname(__file__)
+    os.chdir(os.path.dirname(__file__))
+    os.system('python mainjeu2.py')
+
 
 pygame.init()
 
@@ -24,23 +31,33 @@ dialogue_box = False
 # Pygame GUI manager
 manager = pygame_gui.UIManager((screen_width, screen_height))
 
-# Création buton oui
-button1_width = 100
+# Création button
+button1_width = 120
 button1_height = 30
 button1_x = dialogue_box_x + dialogue_box_width*0.05
 button1_y = dialogue_box_y + dialogue_box_height*0.95 - button1_height
 button1 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(button1_x, button1_y, button1_width, button1_height),
-                                      text='Oui',
+                                      text='*taper sa tête*',
                                       manager=manager)
 
-# Création buton Non
+# Création button
 button2_width = 100
 button2_height = 30
 button2_x = dialogue_box_x + dialogue_box_width*0.95 - button2_width
 button2_y = dialogue_box_y + dialogue_box_height*0.95 - button2_height
 button2 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(button2_x, button2_y, button2_width, button2_height),
-                                      text='Non',
+                                      text='*hmmmmm*',
                                       manager=manager)
+
+# Création button
+button3_width = 100
+button3_height = 30
+button3_x = dialogue_box_x + dialogue_box_width*0.95 - button3_width
+button3_y = dialogue_box_y + dialogue_box_height*0.95 - button3_height
+button3 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(button3_x, button3_y, button3_width, button3_height),
+                                      text='Suivant',
+                                      manager=manager)
+
 
 #création du background
 background = pygame.image.load("Cinematique/img/ruelle_sombre.jpg").convert_alpha()
@@ -49,9 +66,10 @@ background_rect = background.get_rect()
 
 running = True
 clock = pygame.time.Clock()
+indice = 0
+dialogue_box = True
 
 while running:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -59,12 +77,13 @@ while running:
         manager.process_events(event)
 
     keys = pygame.key.get_pressed()
-    dialogue_box = True
+   
 
     # Draw the background
     screen.blit(background, background_rect)
+    if indice == 0 :
+        button3.hide()
 
-    
     if dialogue_box:
         pygame.draw.rect(screen, GRAY, (dialogue_box_x, 
                                         dialogue_box_y, 
@@ -79,7 +98,9 @@ while running:
         manager.draw_ui(screen)
 
 
-        text = "Veux tu aider Bécile ?"
+        bank = ["HAAAAA j’ai plus de corps. Pourquoi ma jambe se trouve à 2 années lumière.\n\n                     VA CHERCHER LE RESTE MAINTENANT!!",
+                "Ok je monte sur ton dos ! Puis je te guide.\n\n                     Je suis un génie !"]
+        text = bank[indice]
         rendered_text = pygame.font.Font(None, 24).render(text, True, WHITE)
         text_rect = rendered_text.get_rect(center=(dialogue_box_x + dialogue_box_width // 2,
                                                    dialogue_box_y + dialogue_box_height // 2))
@@ -88,15 +109,24 @@ while running:
     
         # Check if the buttons are clicked
         if button1.check_pressed():
-            print("Button Oui clicked")
-            # Add your action for button 1 here
-            dialogue_box = False
+            print("Button *frappe* clicked")
+            indice = 0
+            
+
         if button2.check_pressed():
-            print("Button Non clicked")
-            # Add your action for button 2 here
-            dialogue_box = False
+            print("Button *hmm* clicked")
+            indice = 1
+            button2.hide()
+            button1.hide()
+            button3.show()
+
+        if button3.check_pressed() and indice == 1:
+            running = False
+            lancementJ2()
 
 
+        
+        
 
     pygame.display.flip()
     clock.tick(60)
